10f53ed094decb19f904b75b7189f02e0344379c
Михаил Быковский <sosiska_killer@Noutbuk-Mihail.local>
2023-10-26 12:08:00 +0300

    2_nd try commit with time_controller

--- src/TimeController_class/timeControl.hpp
+++ src/TimeController_class/timeControl.hpp
@@ -1,36 +1,22 @@
+#pragma once
+
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include <iostream>
 
-class TimeController {
+class GameTimer {
 public:
-    TimeController() : timeScale(1.0f), currentTime(0.0f), deltaTime(0.0f) {
-        clock.restart();
-    }
-
-    // Обновление времени
-    void update() {
-        deltaTime = clock.restart().asSeconds() * timeScale;
-        currentTime += deltaTime;
-    }
-
-    // Установка масштаба времени (ускорение или замедление)
-    void setTimeScale(float scale) {
-        timeScale = scale;
-    }
-
-    // Получение текущего времени
-    float getCurrentTime() const {
-        return currentTime;
-    }
-
-    // Получение прошедшего времени с масштабом
-    float getDeltaTime() const {
-        return deltaTime;
-    }
+    GameTimer();
+    void Start();
+    void Stop();
+    void SetTimeScale(float scale);
+    float GetTimeScale() const;
+    float Update();
 
 private:
-    sf::Clock clock;     // Часы для измерения времени
-    float timeScale;     // Масштаб времени (1.0f для обычной скорости)
-    float currentTime;   // Текущее время
-    float deltaTime;    // Время, прошедшее с последнего обновления
+    sf::Clock clock;
+    float deltaTime;
+    float timeScale;
+    bool isRunning;
 };
+
